43333811e752b8b9fca3335c269eae311538e12c
XI RPL 2_03_Danendra A <84388509+Danendra09@users.noreply.github.com>
2024-03-06 16:23:23 +0700

    tambah modul 1

---
+++ pertemuan1/guided1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    char op;
+    float num1,num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+
+    cout << "Enter two operands: ";
+    cin >> num1 >> num2;
+
+    switch (op){
+        case '+':
+            cout << "Result: " << num1 + num2;
+            break;
+        case '-':
+            cout << "Result: " << num1 - num2;
+            break;
+        case '*':
+            cout << "Result: " << num1 * num2;
+            break;
+        case '/':
+            if (num2 != 0)
+                cout << "Result: " << num1 / num2;
+            else
+                cout << "Error: Division by zero";
+            break;
+        default:
+            cout << "Error! Operator is not correct";
+    }
+    return 0;
+}
